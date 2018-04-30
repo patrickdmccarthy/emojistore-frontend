868b76ab402552d8db4e9eee26d55e7cbfd038cc
patrickdmccarthy <patrick.d.mccarthy@gmail.com>
2018-04-30 10:35:20 +0200

    use react-ga

--- components/Head.js
+++ components/Head.js
@@ -1,28 +1,36 @@
+import { Component } from 'react'
 import Head from 'next/head'
+import { initGA, logPageView } from '../utils/analytics'
 
-export default () =>
-  <div>
-    <Head>
-      <title>McSocks</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118367130-1"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
+class AppHead extends Component {
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
 
-        gtag('config', 'UA-118367130-1');
-      </script>
-    </Head>
-    <style>{`
-      body {
-        margin: 0;
-      }
+  render() {
+    return(
+      <div>
+        <Head>
+          <title>McSocks</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet" />
+          <style>{`
+          body {
+            margin: 0;
+          }
 
-      * {
-        box-sizing: border-box;
-        font-family: 'Roboto', sans-serif;
-      }
-    `}</style>
-  </div>
+          * {
+            box-sizing: border-box;
+            font-family: 'Roboto', sans-serif;
+          }
+        `}</style>
+        </Head>
+      </div>)
+  }
+}
+
+export default AppHead
--- components/ProductPage.js
+++ components/ProductPage.js
@@ -1,53 +1,44 @@
 import { Component } from 'react'
 import { addToCart } from '../services/product'
 
-class ProductPage extends Component {
-
-  render() {
-    const {addItem, cart, product} = this.props
-
-    return (
-      <div>
-        <div className={"container"}>
-          <div className={"image-container"}>
-            <img src={product.img.medium} />
-          </div>
-          <div className={"details-container"}>
-            <h1>{product.name}</h1>
-            <p>${product.price}</p>
-            <p>{product.text}</p>
-            <div>{
-               cart.CartItems && cart.CartItems.map(item => item.itemId).includes(product.id)
-              ?
-                <button disabled={true}>Already Added</button>
-              :
-                <button onClick={() => addToCart(product, addItem)}>Add to Cart</button>
-                }
-              </div>
+export default ({addItem, cart, product}) => (
+    <div>
+      <div className={"container"}>
+        <div className={"image-container"}>
+          <img src={product.img.medium} />
+        </div>
+        <div className={"details-container"}>
+          <h1>{product.name}</h1>
+          <p>${product.price}</p>
+          <p>{product.text}</p>
+          <div>{
+             cart.CartItems && cart.CartItems.map(item => item.itemId).includes(product.id)
+            ?
+              <button disabled={true}>Already Added</button>
+            :
+              <button onClick={() => addToCart(product, addItem)}>Add to Cart</button>
+              }
             </div>
           </div>
-          <style jsx>{`
-          .container {
-            display: flex;
-            padding: 3em;
-          }
+        </div>
+        <style jsx>{`
+        .container {
+          display: flex;
+          padding: 3em;
+        }
 
-          .image-container {
-            width: 50%;
-          }
+        .image-container {
+          width: 50%;
+        }
 
-          .details-container {
-            width: 50%;
-            padding-left: 8%;
-            padding-right: 8%;
-          }
-          img {
-            max-width: 100%;
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
-
-export default ProductPage
+        .details-container {
+          width: 50%;
+          padding-left: 8%;
+          padding-right: 8%;
+        }
+        img {
+          max-width: 100%;
+        }
+      `}</style>
+  </div>
+)
